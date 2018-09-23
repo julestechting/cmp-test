be5fafc853a0b14fe680444de07ad5fc73ed9b9f
julestechting <julestechconsulting@gmail.com>
2018-09-23 15:37:49 +0000

    Added features to test

--- connect-td/api/chats.js
+++ connect-td/api/chats.js
@@ -1,10 +1,11 @@
-const { checkInt, checkStr, checkJSON } = require('../utils/checkargs')
+const { checkArray, checkInt, checkStr, checkJSON } = require('../utils/checkargs')
 const init = require('./init')
 
 const getChat = async (body) => {
   var result
   try {
-    const client = await init()
+    var client = await init()
+    //console.log(client)
     if(checkJSON(body, 1) &&
       checkStr(body.chat_id)) {
       const chat = await client.fetch({
@@ -21,9 +22,56 @@ const getChat = async (body) => {
     console.log(err)
     result = {type: "error", code: "TL-API-FAIL"}
   }
-  return result;
+  return result
 }
 
+const getSupergroup = async (body) => {
+  var result
+  try {
+    const client = await init()
+    if(checkJSON(body, 1) &&
+      checkInt(body.supergroup_id)) {
+      //Preload the supergroup
+      const pc = await client.fetch({'@type': 'getSupergroup', 'supergroup_id': body.supergroup_id})
+      const chat = await client.fetch({'@type': 'getSupergroupFullInfo', 'supergroup_id': body.supergroup_id})
+      result = {type: "OK", response: chat}
+    }
+    else {
+      result = {type: "error", code: "VALID-FAIL"}
+    }
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+const deleteSupergroup = async (body) => {
+  var result
+  try {
+    const client = await init()
+    if(checkJSON(body, 1) &&
+      checkInt(body.supergroup_id)) {
+      //Preload the supergroup
+      const pc = await client.fetch({'@type': 'getSupergroup', 'supergroup_id': body.supergroup_id})
+      const chat = await client.fetch({'@type': 'deleteSupergroup', 'supergroup_id': body.supergroup_id})
+      result = {type: "OK", response: chat}
+    }
+    else {
+      result = {type: "error", code: "VALID-FAIL"}
+    }
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+
+// chat members are only available for Supergroups and Channels
+// To change: should have the chat_id, check the chat type and if supergroup then get the members
 const getChatMembers = async (body) => {
   var result
   try {
@@ -38,6 +86,33 @@ const getChatMembers = async (body) => {
         'SupergroupMembersFilter': { '@type': 'SupergroupMembersFilterRecent' },
         'offset': '0',
         'limit': '10'
+      })
+      result = {type: "OK", response: chat}
+    }
+    else {
+      result = {type: "error", code: "VALID-FAIL"}
+    }
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+const getMember = async (body) => {
+  var result
+  try {
+    const client = await init()
+    if(checkJSON(body, 2) &&
+      checkInt(body.chat_id) &&
+      checkInt(body.user_id)) {
+      //Preload the supergroup
+      const pc = await client.fetch({'@type': 'getUser', 'user_id': body.user_id})
+      const chat = await client.fetch({
+        '@type': 'getChatMember',
+        'chat_id': body.chat_id,
+        'user_id': body.user_id
       })
       result = {type: "OK", response: chat}
     }
@@ -76,4 +151,157 @@ const setSuperName = async (body) => {
   }
 }
 
-module.exports = { getChat, getChatMembers, setSuperName }
+const removeChatMember = async (body) => {
+  try {
+    const client = await init()
+    if(checkJSON(body, 2) &&
+      checkInt(body.chat_id) &&
+      checkInt(body.user_id)) {
+      //Preload the user
+      const pc = await client.fetch({'@type': 'getUser', 'user_id': body.user_id})
+      const chat = await client.fetch({
+        '@type': 'setChatMemberStatus',
+        'chat_id': body.chat_id,
+        'user_id': body.user_id,
+        'status': {
+          '@type': 'chatMemberStatusLeft'
+        }
+      })
+      //WAS: '@type': 'chatMemberStatusLeft'
+      result = {type: "OK", response: "SUCCESS"}
+    }
+    else {
+      result = {type: "error", code: "VALID-FAIL"}
+    }
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+const banChatMember = async (body) => {
+  try {
+    const client = await init()
+    if(checkJSON(body, 2) &&
+      checkInt(body.chat_id) &&
+      checkInt(body.user_id)) {
+      //Preload the user
+      const pc = await client.fetch({'@type': 'getUser', 'user_id': body.user_id})
+      const chat = await client.fetch({
+        '@type': 'setChatMemberStatus',
+        'chat_id': body.chat_id,
+        'user_id': body.user_id,
+        'status': {
+          '@type': 'chatMemberStatusBanned'
+        }
+      })
+      //WAS: '@type': 'chatMemberStatusLeft'
+      result = {type: "OK", response: "SUCCESS"}
+    }
+    else {
+      result = {type: "error", code: "VALID-FAIL"}
+    }
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+
+/*
+ * Doesn't work!
+ * If me is not contact & user privacy/groups&channel = My Contacts => error 403 USER_PRIVACY_RESTRICTED
+ * If me is not contact & user privacy/groups&channel = Everyone => error 400 USER_NOT_MUTUAL_CONTACT
+ * If me is contact & user privacy/groups&channel = My Contacts => error 400 USER_NOT_MUTUAL_CONTACT
+ * If me is contact & user privacy/groups&channel = Everyone => error 400 USER_NOT_MUTUAL_CONTACT
+ */
+const addChatMember = async (body) => {
+  try {
+    const client = await init()
+    if(checkJSON(body, 2) &&
+      checkInt(body.chat_id) &&
+      checkInt(body.user_id)) {
+      //Preload the user
+      const pc = await client.fetch({'@type': 'getUser', 'user_id': body.user_id})
+      const chat = await client.fetch({
+        '@type': 'addChatMember',
+        'chat_id': body.chat_id,
+        'user_id': body.user_id,
+        'forward_limit': 10 //Not applicable as the group is a supergroup
+      })
+      result = {type: "OK", response: "SUCCESS"}
+    }
+    else {
+      result = {type: "error", code: "VALID-FAIL"}
+    }
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+const getContacts = async () => {
+  try {
+    const client = await init()
+    const contacts = await client.fetch({'@type': 'getContacts'})
+    result = {type: "OK", response: contacts}
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+const importContact = async (body) => {
+  try {
+    const client = await init()
+    if(checkJSON(body, 2) &&
+      checkInt(body.user_id) &&
+      checkStr(body.phone_number)) {
+      //Get user information
+      const pc = await client.fetch({'@type': 'getUser', 'user_id': body.user_id})
+      const contacts = await client.fetch({
+        '@type': 'importContacts',
+        'contacts': [
+          {
+            '@type': 'contact',
+            'phone_number': body.phone_number,
+            'first_name': pc.first_name,
+            'last_name': pc.last_name,
+            'user_id': body.user_id
+          }
+        ]
+      })
+      result = {type: "OK", response: contacts}
+    }
+    else {
+      result = {type: "error", code: "VALID-FAIL"}
+    }
+  }
+  catch (err) {
+    console.log(err)
+    result = {type: "error", code: "TL-API-FAIL"}
+  }
+  return result
+}
+
+module.exports = {
+  addChatMember,
+  banChatMember,
+  deleteSupergroup,
+  getChat,
+  getChatMembers,
+  getContacts,
+  getMember,
+  getSupergroup,
+  importContact,
+  removeChatMember,
+  setSuperName
+}
--- connect-td/api/router.js
+++ connect-td/api/router.js
@@ -1,14 +1,7 @@
 const express = require('express'),
   router = express.Router(),
-  init = require('./init'),
-  { getChat, getChatMembers, setSuperName } = require('./chats');
+  { addChatMember, banChatMember, deleteSupergroup, getChat, getChatMembers, getContacts, getMember, getSupergroup, importContact, removeChatMember, setSuperName } = require('./chats');
 
-/*
-router.get('/init', async (req, res) => {
-  //return res.json(await init());
-  return res.json((await init()).appDir);
-});
-*/
 router.post('/getchat', async (req, res) => {
   return res.json(await getChat(req.body));
 })
@@ -17,10 +10,41 @@ router.post('/getchatmembers', async (req, res) => {
   return res.json(await getChatMembers(req.body));
 })
 
+router.post('/getmember', async (req, res) => {
+  return res.json(await getMember(req.body));
+})
+
+router.post('/banmember', async (req, res) => {
+  return res.json(await banChatMember(req.body));
+})
+
+router.post('/getsuper', async (req, res) => {
+  return res.json(await getSupergroup(req.body));
+})
+
 router.post('/setsupername', async (req, res) => {
   return res.json(await setSuperName(req.body));
 })
 
+router.post('/removemember', async (req, res) => {
+  return res.json(await removeChatMember(req.body));
+})
+
+router.post('/addmember', async (req, res) => {
+  return res.json(await addChatMember(req.body));
+})
+
+router.post('/delsuper', async (req, res) => {
+  return res.json(await deleteSupergroup(req.body));
+})
+
+router.get('/getcontacts', async (req, res) => {
+  return res.json(await getContacts());
+})
+
+router.post('/importcontact', async (req, res) => {
+  return res.json(await importContact(req.body));
+})
 
 // Default
 router.get('/', (req, res) => {
